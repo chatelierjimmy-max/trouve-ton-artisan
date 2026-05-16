3a2affe80b369e255ae0e2a28c3405253d668d76
chatelierjimmy <chatelierjimmy@gmail.com>
2026-05-16 18:26:00 +0200

    amélioration page 404 et accessibilité

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,15 +1,36 @@
 import { Link } from "react-router-dom";
 
+import Seo from "../components/Seo";
+
+import notFoundImage from "../assets/404.png";
+
 function NotFound() {
   return (
-    <section className="container py-5 text-center">
-      <h1>Page non trouvée</h1>
-      <p>La page demandée n’existe pas.</p>
+    <>
+      <Seo
+        title="Page non trouvée"
+        description="La page demandée est introuvable."
+      />
 
-      <Link to="/" className="btn btn-primary">
-        Retour à l’accueil
-      </Link>
-    </section>
+      <section className="container py-5 text-center">
+        <img
+          src={notFoundImage}
+          alt="Page non trouvée"
+          className="img-fluid mb-4"
+          style={{
+            maxWidth: "320px",
+          }}
+        />
+
+        <h1 className="display-4 mb-3">Page non trouvée</h1>
+
+        <p className="lead mb-4">La page demandée n’existe pas.</p>
+
+        <Link to="/" className="btn btn-primary">
+          Retour à l’accueil
+        </Link>
+      </section>
+    </>
   );
 }
 
